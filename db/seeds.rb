d3b1dd26cfe5e156dc1c3cb6e0e9e4617445e717
Irvin Etori <irvin.etori@gmail.com>
2023-02-24 15:35:12 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
 
 Bookmark.destroy_all
 Movie.destroy_all
@@ -16,8 +17,13 @@ movie2 = Movie.create(title: "The Shawshank Redemption", overview: "Framed in th
 movie3 = Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 movie4 = Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 puts 'Creating list...'
-list1 = List.create(name: 'Fantastic')
-list2 = List.create(name: 'Thriller')
+file1 = URI.open("https://res.cloudinary.com/dbglim49p/image/upload/v1677247608/dwayne-johnson-roadblock-film-cinema-movie-man-flame-fire-sp_aue9cg.jpg")
+file2 = URI.open("https://res.cloudinary.com/dbglim49p/image/upload/v1677247608/thumb-1920-601827_uvv0dl.jpg")
+list1 = List.new(name: 'Action')
+list1.photo.attach(io: file1, filename: "dwayne.png", content_type: "image/png")
+list1.save
+list2 = List.new(name: 'Fantastic')
+list2.photo.attach(io: file2, filename: "fourfantastic.png", content_type: "image/png")
 
 puts 'Create Bookmarks'
 Bookmark.create(movie: movie1, list: list1, comment: 'ceci est un commentaire')
